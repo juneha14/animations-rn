1c9bc33cd0c60b15ebdff82f08ded132ae40fefa
June Ha <june.ha14@gmail.com>
2022-01-19 18:26:52 -0800

    Play around with velocity induced translate y offset

--- src/BottomSheet/BottomSheet.tsx
+++ src/BottomSheet/BottomSheet.tsx
@@ -1,23 +1,29 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import {
   Dimensions,
   Pressable,
   SafeAreaView,
-  ScrollView,
   StyleSheet,
   Text,
+  ScrollView,
   View,
 } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import {
+  Gesture,
+  GestureDetector,
+  TextInput,
+} from "react-native-gesture-handler";
+import { panGestureHandlerCustomNativeProps } from "react-native-gesture-handler/lib/typescript/handlers/PanGestureHandler";
 import Animated, {
-  measure,
+  block,
   runOnJS,
   runOnUI,
   useAnimatedReaction,
   useAnimatedRef,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
-  useDerivedValue,
   useSharedValue,
+  withDecay,
   withSpring,
   withTiming,
 } from "react-native-reanimated";
@@ -52,15 +58,15 @@ const { width, height } = Dimensions.get("window");
 
 const SNAP_POINTS = {
   hidden: height,
-  opened: height * 0.2,
+  opened: height * 0.35,
   full: height * 0.9,
 };
 
-const SNAP_THRESHOLD = height / 2;
+const SNAP_UP_THRESHOLD = height * 0.6;
 
 // TODO:
-// velocity based open/close
-// put gesture on entire scrollView - don't active scrollView until it is fully opened
+// put gesture on entire scrollView
+// make it pretty
 const Sheet = ({
   open,
   onDismissSheet,
@@ -72,6 +78,7 @@ const Sheet = ({
   const startOffsetY = useSharedValue(0);
 
   useEffect(() => {
+    // Open or close bottom sheet on external button press
     offsetY.value = withTiming(open ? -SNAP_POINTS.opened : 0);
   }, [offsetY, open]);
 
@@ -82,30 +89,47 @@ const Sheet = ({
     .onUpdate((e) => {
       offsetY.value = e.translationY + startOffsetY.value;
     })
-    .onEnd(() => {
+    .onEnd((e) => {
       const absOffsetY = Math.abs(offsetY.value);
+      const absVelY = Math.abs(e.velocityY);
+      const panVelocityOffset =
+        absOffsetY + absVelY * 0.074 * (e.velocityY < 0 ? 1 : -1);
+      const comp = absOffsetY > absVelY ? absOffsetY : panVelocityOffset;
 
-      if (absOffsetY > SNAP_THRESHOLD) {
+      let destSnapPoint = -SNAP_POINTS.full;
+
+      // TODO: Think of a better way to determine the destination snap point
+      // Possibly use redash library's snapPoints() function?
+      if (comp > SNAP_UP_THRESHOLD) {
         // Open bottom sheet fully
-        offsetY.value = withTiming(-SNAP_POINTS.full);
-      } else if (
-        absOffsetY <= SNAP_THRESHOLD &&
-        absOffsetY > SNAP_POINTS.opened
-      ) {
+        destSnapPoint = -SNAP_POINTS.full;
+      } else if (comp <= SNAP_UP_THRESHOLD && comp >= SNAP_POINTS.opened) {
         // Collapse to original open position
-        offsetY.value = withTiming(-SNAP_POINTS.opened);
-      } else {
+        destSnapPoint = -SNAP_POINTS.opened;
+      } else if (comp < SNAP_POINTS.hidden) {
         // Dismiss bottom sheet entirely
-        offsetY.value = withTiming(0);
+        destSnapPoint = 0;
+
+        // AWARE: This causes a bit of a lag
         runOnJS(onDismissSheet)();
       }
+
+      offsetY.value = withSpring(destSnapPoint, {
+        overshootClamping: true,
+        damping: 50,
+        mass: 0.3,
+        stiffness: 121.6,
+        restSpeedThreshold: 0.3,
+        restDisplacementThreshold: 0.3,
+      });
     });
 
   const animatedStyle = useAnimatedStyle(() => {
+    const clampedTranslateY = Math.max(offsetY.value, -SNAP_POINTS.full);
     return {
       transform: [
         {
-          translateY: offsetY.value,
+          translateY: clampedTranslateY,
         },
       ],
     };
@@ -114,13 +138,15 @@ const Sheet = ({
   return (
     <Animated.View style={[styles.sheetContainer, animatedStyle]}>
       <GestureDetector gesture={panGesture}>
-        <View style={[styles.sheetNavBar]} />
+        <View>
+          <View style={styles.sheetNavBar} />
+          <ScrollView scrollEnabled={false}>
+            {[...Array(100).keys()].map((val) => {
+              return <Item key={val} val={val} />;
+            })}
+          </ScrollView>
+        </View>
       </GestureDetector>
-      <ScrollView>
-        {[...Array(100).keys()].map((val, index) => {
-          return <Item key={val} val={val} />;
-        })}
-      </ScrollView>
     </Animated.View>
   );
 };
