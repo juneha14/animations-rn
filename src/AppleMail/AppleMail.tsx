3e1c431c3e2fd91aa8d4bc24afe0a3e757eaab2c
June Ha <june.ha14@gmail.com>
2022-02-15 09:04:23 -0800

    Get general action animation working

--- src/AppleMail/AppleMail.tsx
+++ src/AppleMail/AppleMail.tsx
@@ -1,80 +1,230 @@
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
-import { Colors, Spacing } from "../utils";
+import { Dimensions, Text, View } from "react-native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+import {
+  Gesture,
+  GestureDetector,
+  ScrollView,
+} from "react-native-gesture-handler";
+import { Colors, snapPoints, Spacing } from "../utils";
 import { Ionicons } from "@expo/vector-icons";
+
+// Vertical scroll not working when scrolling within row
+// Multiple action row buttons
+// Search bar scale animation
+// Rearrange rows
 
 export const AppleMail: React.FC = () => {
   return (
     <ScrollView
       style={{ flex: 1, backgroundColor: Colors.SurfaceForeground }}
       contentContainerStyle={{
-        paddingHorizontal: Spacing.xl,
         paddingVertical: Spacing.defaultMargin,
         backgroundColor: Colors.SurfaceForeground,
       }}
     >
       <Header />
-      {[...Array(20).keys()].map((val) => {
+      {[...Array(10).keys()].map((val) => {
         return <Row key={val} val={val} />;
       })}
     </ScrollView>
   );
 };
 
+const { width } = Dimensions.get("window");
+const SNAP_POINTS = [0, -80, -width * 0.7];
+
 const Row = ({ val }: { val: number }) => {
+  const startOffsetX = useSharedValue(0);
+  const offsetX = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .averageTouches(true)
+    .onStart(() => {
+      startOffsetX.value = offsetX.value;
+    })
+    .onUpdate((e) => {
+      offsetX.value = startOffsetX.value + e.translationX;
+    })
+    .onEnd((e) => {
+      const snapPoint = snapPoints(offsetX.value, e.velocityX, SNAP_POINTS);
+      offsetX.value = withTiming(snapPoint);
+    });
+
+  const aStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: offsetX.value,
+        },
+      ],
+    };
+  });
+
   return (
-    <>
-      <View style={{ height: 1, backgroundColor: Colors.BorderSubdued }} />
-      <View
-        style={{
-          paddingVertical: Spacing.m,
-        }}
-      >
-        {/* Contact and date container */}
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "space-between",
-            marginBottom: 5,
-          }}
-        >
-          <Text
+    <GestureDetector gesture={panGesture}>
+      <View style={{ flexDirection: "row" }}>
+        <View>
+          {/* Divider */}
+          <View
             style={{
-              fontWeight: "500",
-              fontSize: 17,
+              height: 1,
+              marginLeft: Spacing.xl,
+              backgroundColor: Colors.BorderSubdued,
             }}
-          >{`John Doe ${val}`}</Text>
-          <Text style={{ color: Colors.TextSubdued }}>Friday</Text>
+          />
+
+          <Animated.View
+            style={[
+              {
+                paddingVertical: Spacing.m,
+                paddingRight: Spacing.l,
+                paddingLeft: Spacing.xl,
+              },
+              aStyle,
+            ]}
+          >
+            {/* Contact and date container */}
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                marginBottom: 5,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "500",
+                  fontSize: 17,
+                }}
+              >{`John Doe ${val}`}</Text>
+              <Text style={{ color: Colors.TextSubdued }}>Friday</Text>
+            </View>
+
+            {/* Mail title and content preview */}
+            <View>
+              <Text
+                style={{
+                  fontSize: 15,
+                  marginBottom: 5,
+                  color: Colors.TextNeutral,
+                }}
+              >
+                Money Team Update: January 2022
+              </Text>
+              <Text
+                style={{ color: Colors.TextSubdued, fontSize: 15 }}
+                numberOfLines={2}
+              >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </Text>
+            </View>
+          </Animated.View>
         </View>
 
-        {/* Mail title and content preview */}
-        <View>
-          <Text
-            style={{
-              fontSize: 15,
-              marginBottom: 5,
-              color: Colors.TextNeutral,
-            }}
-          >
-            Money Team Update: January 2022
-          </Text>
-          <Text
-            style={{ color: Colors.TextSubdued, fontSize: 15 }}
-            numberOfLines={2}
-          >
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Text>
-        </View>
+        {/* Row action button container */}
+        <RowActionButton offsetX={offsetX} />
       </View>
-    </>
+    </GestureDetector>
+  );
+};
+
+const RowActionButton = ({
+  offsetX,
+}: {
+  offsetX: Animated.SharedValue<number>;
+}) => {
+  const aStyle = useAnimatedStyle(() => {
+    const inputRange = [SNAP_POINTS[2], SNAP_POINTS[1], SNAP_POINTS[0]];
+
+    const initialTranslateX = -SNAP_POINTS[2] + SNAP_POINTS[1];
+    const offsetTranslateXFromScale = initialTranslateX - initialTranslateX / 2;
+
+    // const translateX = interpolate(
+    //   offsetX.value,
+    //   inputRange,
+    //   [-offsetTranslateXFromScale, 0, -SNAP_POINTS[1]],
+    //   Extrapolate.CLAMP
+    // )
+    const translateX = interpolate(
+      offsetX.value,
+      inputRange,
+      [0, 0, -SNAP_POINTS[1]],
+      Extrapolate.CLAMP
+    );
+
+    const scaleX = interpolate(
+      offsetX.value,
+      inputRange,
+      [SNAP_POINTS[2] / SNAP_POINTS[1], 1, 1],
+      Extrapolate.CLAMP
+    );
+
+    const width = interpolate(
+      offsetX.value,
+      inputRange,
+      [-SNAP_POINTS[2], -SNAP_POINTS[1], -SNAP_POINTS[1]],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      transform: [{ translateX: translateX }, { scaleX: 1 }],
+      width,
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          position: "absolute",
+          right: 0,
+          justifyContent: "center",
+          width: 80,
+          height: "100%",
+          //   paddingLeft: Spacing.l,
+          backgroundColor: "red",
+        },
+        aStyle,
+      ]}
+    >
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: 80,
+        }}
+      >
+        <Ionicons
+          name="ios-trash-outline"
+          color={Colors.TextOnSurfacePrimary}
+          size={24}
+        />
+        <Text
+          style={{
+            color: Colors.TextOnSurfacePrimary,
+            fontWeight: "600",
+            fontSize: 15,
+          }}
+        >
+          Trash
+        </Text>
+      </View>
+    </Animated.View>
   );
 };
 
 const Header = () => {
   return (
-    <View style={{ marginBottom: Spacing.m }}>
+    <View style={{ paddingHorizontal: Spacing.xl, marginBottom: Spacing.m }}>
       <Text style={{ fontWeight: "700", fontSize: 30 }}>Inbox</Text>
       {/* Search bar */}
       <View
