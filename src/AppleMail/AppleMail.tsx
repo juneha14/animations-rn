9ecd2c20802e39df751f448c2509e0cb0e510fd6
June Ha <june.ha14@gmail.com>
2022-02-18 11:01:31 -0800

    Get simplified single trash action button working with icon translation when threshold is reached

--- src/AppleMail/AppleMail.tsx
+++ src/AppleMail/AppleMail.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Dimensions, Text, View } from "react-native";
+import { Dimensions, Text, View, StyleSheet } from "react-native";
 import Animated, {
   Extrapolate,
   interpolate,
   useAnimatedReaction,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
+  withDelay,
   withSpring,
   withTiming,
 } from "react-native-reanimated";
@@ -16,6 +18,7 @@ import {
 } from "react-native-gesture-handler";
 import { Colors, snapPoints, Spacing } from "../utils";
 import { Ionicons } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 // Multiple action row buttons
 // Search bar scale animation
@@ -38,7 +41,7 @@ export const AppleMail: React.FC = () => {
 };
 
 const { width } = Dimensions.get("window");
-const SNAP_POINTS = [0, -80 * 3, -width];
+const SNAP_POINTS = [0, -80, -width];
 
 const Row = ({ val }: { val: number }) => {
   const startOffsetX = useSharedValue(0);
@@ -55,11 +58,11 @@ const Row = ({ val }: { val: number }) => {
       offsetX.value = startOffsetX.value + e.translationX;
     })
     .onEnd((e) => {
-      let snapPoint = snapPoints(offsetX.value, e.velocityX, SNAP_POINTS);
+      const snapPoint = snapPoints(offsetX.value, e.velocityX, SNAP_POINTS);
 
-      if (snapPoint === SNAP_POINTS[2] && offsetX.value > snapPoint) {
-        snapPoint = SNAP_POINTS[1];
-      }
+      //   if (snapPoint === SNAP_POINTS[2] && offsetX.value > snapPoint) {
+      //     snapPoint = SNAP_POINTS[1];
+      //   }
 
       offsetX.value = withSpring(snapPoint, {
         overshootClamping: true,
@@ -77,7 +80,7 @@ const Row = ({ val }: { val: number }) => {
     return {
       transform: [
         {
-          translateX,
+          translateX: offsetX.value,
         },
       ],
     };
@@ -85,75 +88,339 @@ const Row = ({ val }: { val: number }) => {
 
   return (
     <GestureDetector gesture={panGesture}>
-      <View style={{ flexDirection: "row" }}>
-        {/* Row content container */}
-        <View>
-          {/* Divider */}
+      {/* <View style={{ flexDirection: "row" }}> */}
+      {/* Row content container */}
+      <View>
+        {/* Divider */}
+        <View
+          style={{
+            height: 1,
+            marginLeft: Spacing.xl,
+            backgroundColor: Colors.BorderSubdued,
+          }}
+        />
+
+        {/* Content container */}
+        <Animated.View
+          style={[
+            {
+              paddingVertical: Spacing.m,
+              paddingRight: Spacing.l,
+              paddingLeft: Spacing.xl,
+            },
+            aStyle,
+          ]}
+        >
+          {/* Contact and date */}
           <View
             style={{
-              height: 1,
-              marginLeft: Spacing.xl,
-              backgroundColor: Colors.BorderSubdued,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              marginBottom: 5,
             }}
-          />
-
-          {/* Content container */}
-          <Animated.View
-            style={[
-              {
-                paddingVertical: Spacing.m,
-                paddingRight: Spacing.l,
-                paddingLeft: Spacing.xl,
-              },
-              aStyle,
-            ]}
           >
-            {/* Contact and date */}
-            <View
+            <Text
               style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "space-between",
+                fontWeight: "500",
+                fontSize: 17,
+              }}
+            >{`John Doe ${val}`}</Text>
+            <Text style={{ color: Colors.TextSubdued }}>Friday</Text>
+          </View>
+
+          {/* Mail title and content preview */}
+          <View>
+            <Text
+              style={{
+                fontSize: 15,
                 marginBottom: 5,
+                color: Colors.TextNeutral,
               }}
             >
-              <Text
-                style={{
-                  fontWeight: "500",
-                  fontSize: 17,
-                }}
-              >{`John Doe ${val}`}</Text>
-              <Text style={{ color: Colors.TextSubdued }}>Friday</Text>
-            </View>
+              Money Team Update: January 2022
+            </Text>
+            <Text
+              style={{ color: Colors.TextSubdued, fontSize: 15 }}
+              numberOfLines={2}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Text>
+          </View>
+        </Animated.View>
 
-            {/* Mail title and content preview */}
-            <View>
-              <Text
-                style={{
-                  fontSize: 15,
-                  marginBottom: 5,
-                  color: Colors.TextNeutral,
-                }}
-              >
-                Money Team Update: January 2022
-              </Text>
-              <Text
-                style={{ color: Colors.TextSubdued, fontSize: 15 }}
-                numberOfLines={2}
-              >
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </Text>
-            </View>
-          </Animated.View>
-        </View>
-
-        {/* Row action button container */}
-        <RowActionButton offsetX={offsetX} type="more" index={2} />
-        <RowActionButton offsetX={offsetX} type="move" index={1} />
-        <RowActionButton offsetX={offsetX} type="trash" index={0} />
+        <DeleteAction offsetX={offsetX} />
+        {/* <RowActionContainer offsetX={offsetX} /> */}
       </View>
+
+      {/* Row action button container */}
+      {/* <RowActionButton offsetX={offsetX} type="more" index={2} />
+        <RowActionButton offsetX={offsetX} type="move" index={1} />
+        <RowActionButton offsetX={offsetX} type="trash" index={0} /> */}
+      {/* </View> */}
     </GestureDetector>
+  );
+};
+
+const DeleteAction = ({
+  offsetX,
+}: {
+  offsetX: Animated.SharedValue<number>;
+}) => {
+  const reachedThreshold = useSharedValue(false);
+  const iconX = useSharedValue(0);
+  const animationComplete = useSharedValue(true);
+
+  useAnimatedReaction(
+    () => Math.abs(offsetX.value),
+    (x) => {
+      if (x >= -SNAP_POINTS[2] - Spacing.xl) {
+        if (!reachedThreshold.value) {
+          reachedThreshold.value = true;
+          animationComplete.value = false;
+
+          iconX.value = withTiming(
+            -x + 60,
+            { duration: 100 },
+            () => (animationComplete.value = true)
+          );
+        } else if (x > 201 && animationComplete.value) {
+          //   iconX.value = -x + 60;
+          iconX.value = withTiming(-x + 60, { duration: 30 });
+        }
+      } else {
+        if (reachedThreshold.value) {
+          reachedThreshold.value = false;
+          iconX.value = withTiming(0, { duration: 150 });
+        }
+      }
+    }
+  );
+
+  const iconAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: iconX.value,
+        },
+      ],
+    };
+  });
+
+  const aStyle = useAnimatedStyle(() => {
+    const translateX = interpolate(
+      offsetX.value,
+      [-80, 0],
+      [0, 80],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      width: Math.max(Math.abs(offsetX.value), 80),
+      transform: [{ translateX }],
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          right: 0,
+          backgroundColor: "red",
+        },
+        aStyle,
+      ]}
+    >
+      <Animated.View
+        style={[
+          {
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            // width: "100%",
+            // left: 30,
+            right: 25,
+            justifyContent: "center",
+            alignItems: "center",
+            // backgroundColor: "pink",
+          },
+          iconAnimatedStyle,
+        ]}
+      >
+        <Ionicons
+          name="ios-trash-outline"
+          size={25}
+          color={Colors.TextOnSurfacePrimary}
+        />
+      </Animated.View>
+    </Animated.View>
+  );
+};
+
+const RowActionContainer = ({
+  offsetX,
+}: {
+  offsetX: Animated.SharedValue<number>;
+}) => {
+  const aStyle = useAnimatedStyle(() => {
+    const scaleFactor = Math.abs(SNAP_POINTS[2] / SNAP_POINTS[1]);
+
+    const width = interpolate(
+      offsetX.value,
+      [
+        SNAP_POINTS[2],
+        SNAP_POINTS[2] + Spacing.xl + 2,
+        SNAP_POINTS[1],
+        SNAP_POINTS[0],
+      ],
+      [-SNAP_POINTS[2], 80 * 3 * scaleFactor, 80 * 3, 0],
+      Extrapolate.CLAMP
+    );
+    return {
+      width: width,
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          //   ...StyleSheet.absoluteFillObject,
+          flexDirection: "row",
+          //   flex: 1,
+          //   justifyContent: "flex-end",
+          position: "absolute",
+          right: 0,
+          height: "100%",
+          backgroundColor: "pink",
+        },
+        aStyle,
+      ]}
+    >
+      <RowAction type="more" offsetX={offsetX} />
+      <RowAction type="move" offsetX={offsetX} />
+      <RowAction type="trash" offsetX={offsetX} />
+    </Animated.View>
+  );
+};
+
+const RowAction = ({
+  type,
+  offsetX,
+}: {
+  type: keyof typeof RowActions;
+  offsetX: Animated.SharedValue<number>;
+}) => {
+  const reachedThreshold = useSharedValue(false);
+  const flex1 = useSharedValue(1);
+  const animationInProgress = useSharedValue(false);
+  const scaleFactor = Math.abs(SNAP_POINTS[2] / SNAP_POINTS[1]);
+
+  useAnimatedReaction(
+    () => offsetX.value,
+    (x) => {
+      if (!animationInProgress.value) {
+        if (x <= SNAP_POINTS[2] + Spacing.xl) {
+          animationInProgress.value = true;
+          reachedThreshold.value = true;
+
+          //   flex1.value = withSpring(
+          //     80 * 2 * scaleFactor,
+          //     {
+          //       overshootClamping: true,
+          //       damping: 50,
+          //       mass: 0.3,
+          //       stiffness: 121.6,
+          //       restSpeedThreshold: 0.3,
+          //       restDisplacementThreshold: 0.3,
+          //     },
+          //     () => (animationInProgress.value = false)
+          //   );
+
+          flex1.value = withTiming(
+            80 * 3 * scaleFactor,
+            { duration: 1000 },
+            () => (animationInProgress.value = false)
+          );
+        } else {
+          animationInProgress.value = true;
+          reachedThreshold.value = false;
+
+          flex1.value = withTiming(
+            80,
+            { duration: 1000 },
+            () => (animationInProgress.value = false)
+          );
+        }
+      }
+    }
+  );
+
+  const aStyle = useAnimatedStyle(() => {
+    if (type !== "trash") return {};
+
+    const f = interpolate(
+      offsetX.value,
+      [
+        // SNAP_POINTS[2] + Spacing.xl,
+        SNAP_POINTS[2] + Spacing.xl + 2,
+        SNAP_POINTS[1],
+        SNAP_POINTS[0],
+      ],
+      [90, 1, 1],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      //   flex: flex1.value,
+      //   transform: [
+      //     {
+      //       scaleX: flex1.value,
+      //     },
+      //   ],
+      width: flex1.value,
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          flex: 1,
+          justifyContent: "center",
+          backgroundColor: RowActions[type].color,
+          //   width: 80,
+        },
+        aStyle,
+      ]}
+    >
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          width: 80,
+        }}
+      >
+        {/* <Ionicons
+          name={RowActions[type].icon as keyof typeof Ionicons.glyphMap}
+          color={Colors.TextOnSurfacePrimary}
+          size={24}
+        />
+        <Text
+          style={{
+            color: Colors.TextOnSurfacePrimary,
+            fontWeight: "600",
+            fontSize: 15,
+          }}
+        >
+          {RowActions[type].name}
+        </Text> */}
+      </View>
+    </Animated.View>
   );
 };
 
@@ -168,6 +435,7 @@ const RowActionButton = ({
 }) => {
   const width = useSharedValue(0);
   const reachedThreshold = useSharedValue(false);
+  const animationComplete = useSharedValue(true);
 
   useAnimatedReaction(
     () => offsetX.value,
@@ -179,14 +447,8 @@ const RowActionButton = ({
         const threshold = SNAP_POINTS[2] + Spacing.xl + 2;
         const interpolated = interpolate(
           translateX,
-          [
-            SNAP_POINTS[2],
-            SNAP_POINTS[2] + Spacing.xl,
-            SNAP_POINTS[2] + Spacing.xl + 2,
-            SNAP_POINTS[1],
-            SNAP_POINTS[0],
-          ],
-          [-SNAP_POINTS[2], -SNAP_POINTS[2], 80 * scaleFactor, 80, 0],
+          [SNAP_POINTS[2] + Spacing.xl + 2, SNAP_POINTS[1], SNAP_POINTS[0]],
+          [80 * scaleFactor, 80, 0],
           Extrapolate.CLAMP
         );
 
@@ -197,9 +459,19 @@ const RowActionButton = ({
           //   if (reachedThreshold.value) return;
           //   reachedThreshold.value = true;
           //   width.value = withTiming(80 * 3 * scaleFactor, { duration: 300 });
-        } else if (reachedThreshold.value) {
-          reachedThreshold.value = false;
-        } else {
+        } else if (
+          reachedThreshold.value &&
+          translateX > threshold
+          // translateX < SNAP_POINTS[1]
+        ) {
+          if (!animationComplete.value) return;
+          animationComplete.value = false;
+          animationComplete.value = true;
+          width.value = withTiming(interpolated, { duration: 200 }, () => {
+            animationComplete.value = true;
+            reachedThreshold.value = false;
+          });
+        } else if (animationComplete.value) {
           width.value = interpolated;
           //   reachedThreshold.value = false;
           //   width.value = interpolate(
