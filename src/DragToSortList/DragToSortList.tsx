4ee1835690cb5fa30a3ebedcc3a251afbe05beaa
June Ha <june.ha14@gmail.com>
2022-02-23 14:54:38 -0800

    Simplify logic by checking if current non-dragging row is inside dragged zone (defined as startDragPos - endDragPos). This makes fast dragging work properly

--- src/DragToSortList/DragToSortList.tsx
+++ src/DragToSortList/DragToSortList.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Image, Text, View } from "react-native";
 import Animated, {
   runOnJS,
-  runOnUI,
   useAnimatedReaction,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import {
+  Gesture,
+  GestureDetector,
+  RotationGestureHandler,
+} from "react-native-gesture-handler";
 import { Colors, Spacing } from "../utils";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -44,6 +47,10 @@ export const DragToSortList: React.FC = () => {
 const ROW_HEIGHT = 60;
 const NAV_BAR_HEIGHT = 111;
 
+// onEnd gesture should snap the dragged row into proper position
+// onEnd gesture for dragged row outside the view's bounds should snap into position
+// scrolling row
+
 const Row = ({
   index,
   title,
@@ -61,76 +68,110 @@ const Row = ({
 }) => {
   const activeRange = [index * ROW_HEIGHT, (index + 1) * ROW_HEIGHT];
   const offsetY = useSharedValue(0);
-  const inProgress = useSharedValue(false);
 
-  const startDragY = useSharedValue(0);
+  const dragStartY = useSharedValue(0);
   const dragY = useSharedValue(0);
   const dragging = useSharedValue(false);
   const zIndex = useSharedValue(0);
 
-  const move = (y: number) => {
-    "worklet";
-    offsetY.value = withTiming(
-      y,
-      { duration: 50 },
-      () => (inProgress.value = false)
-    );
-  };
-
   useAnimatedReaction(
     () => Math.abs(absY.value - NAV_BAR_HEIGHT),
     (y) => {
-      const top = activeRange[0] + offsetY.value;
-      const bottom = activeRange[1] + offsetY.value;
+      if (dragIndex.value === index || dragIndex.value === -1) return;
 
-      if (
-        dragIndex.value !== index &&
-        dragIndex.value !== -1 &&
-        y >= top &&
-        y <= bottom
-      ) {
-        const mid = top + ROW_HEIGHT / 2;
+      const dragStartPos = dragIndex.value * ROW_HEIGHT;
+      const dragEndPos = y;
+      const dragDelta = dragStartPos - dragEndPos;
 
-        if (velY.value > 0) {
-          // Moving down
-          if (y + ROW_HEIGHT > mid) {
-            if (inProgress.value) return;
-            inProgress.value = true;
+      const rowMidPos = activeRange[0] + ROW_HEIGHT / 2;
 
-            if (offsetY.value === 0) {
-              //   offsetY.value = withTiming(-ROW_HEIGHT);
-              //   offsetY.value = -ROW_HEIGHT;
-              move(-ROW_HEIGHT);
-            } else if (offsetY.value === ROW_HEIGHT) {
-              //   offsetY.value = withTiming(0);
-              //   offsetY.value = 0;
-              move(0);
-            }
-          }
+      // Moved drag row up, so move my (index) row down
+      if (dragDelta > 0) {
+        const isRowInsideDraggedZone =
+          dragEndPos <= rowMidPos && dragStartPos >= rowMidPos;
+
+        if (isRowInsideDraggedZone) {
+          offsetY.value = withTiming(ROW_HEIGHT);
         } else {
-          // Moving up
-          if (y <= mid) {
-            if (inProgress.value) return;
-            inProgress.value = true;
+          offsetY.value = withTiming(0);
+        }
+      }
+      // Moved drag row down, so move my (index) row up
+      else {
+        const isRowInsideDraggedZone =
+          dragStartPos <= rowMidPos && dragEndPos >= rowMidPos;
 
-            if (offsetY.value === 0) {
-              //   offsetY.value = withTiming(ROW_HEIGHT);
-              //   offsetY.value = ROW_HEIGHT;
-              move(ROW_HEIGHT);
-            } else if (offsetY.value === -ROW_HEIGHT) {
-              //   offsetY.value = withTiming(0);
-              //   offsetY.value = 0;
-              move(0);
-            }
-          }
+        if (isRowInsideDraggedZone) {
+          offsetY.value = withTiming(-ROW_HEIGHT);
+        } else {
+          offsetY.value = withTiming(0);
         }
       }
     }
   );
 
+  //   useAnimatedReaction(
+  //     () => Math.abs(absY.value - NAV_BAR_HEIGHT),
+  //     (y) => {
+  //       if (dragIndex.value === index || dragIndex.value === -1) return;
+
+  //       const top = activeRange[0] + offsetY.value;
+  //       const bottom = activeRange[1] + offsetY.value;
+  //       const mid = top + ROW_HEIGHT / 2;
+
+  //       if (y >= top && y <= bottom) {
+  //         if (velY.value > 0) {
+  //           // Moving down
+  //           if (y + ROW_HEIGHT > mid) {
+  //             if (offsetY.value === 0) {
+  //               offsetY.value = withTiming(-ROW_HEIGHT);
+  //             } else if (offsetY.value === ROW_HEIGHT) {
+  //               offsetY.value = withTiming(0);
+  //             }
+  //           }
+  //         } else {
+  //           // Moving up
+  //           if (y <= mid) {
+  //             if (offsetY.value === 0) {
+  //               offsetY.value = withTiming(ROW_HEIGHT);
+  //             } else if (offsetY.value === -ROW_HEIGHT) {
+  //               offsetY.value = withTiming(0);
+  //             }
+  //           }
+  //         }
+  //       } else {
+  //         if (velY.value < -500) {
+  //           // Dragged up very quickly
+  //           console.log(
+  //             "========== File: DragToSortList.tsx, Line: 118 =========="
+  //           );
+  //           if (y < mid) {
+  //             if (offsetY.value === 0) {
+  //               offsetY.value = withTiming(ROW_HEIGHT, { duration: 100 });
+  //             } else if (offsetY.value === -ROW_HEIGHT) {
+  //               offsetY.value = withTiming(0, { duration: 100 });
+  //             }
+  //           }
+  //         } else if (velY.value > 500) {
+  //           console.log(
+  //             "========== File: DragToSortList.tsx, Line: 126 =========="
+  //           );
+  //           // Dragged down very quickly
+  //           if (y < mid) {
+  //             if (offsetY.value === 0) {
+  //               offsetY.value = withTiming(-ROW_HEIGHT, { duration: 100 });
+  //             } else if (offsetY.value === ROW_HEIGHT) {
+  //               offsetY.value = withTiming(0, { duration: 100 });
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   );
+
   const panGesture = Gesture.Pan()
     .onStart(() => {
-      startDragY.value = dragY.value;
+      dragStartY.value = dragY.value;
       dragging.value = true;
       zIndex.value = 1;
       runOnJS(onBeginDrag)();
@@ -139,12 +180,12 @@ const Row = ({
       absY.value = e.absoluteY;
       velY.value = e.velocityY;
 
-      dragY.value = startDragY.value + e.translationY;
+      dragY.value = dragStartY.value + e.translationY;
     })
     .onEnd(() => {
-      //   dragY.value = withTiming(0, { duration: 0 }, () => {
-      //     zIndex.value = 0;
-      //   });
+      dragY.value = withTiming(0, { duration: 300 }, () => {
+        zIndex.value = 0;
+      });
       dragging.value = false;
     });
 
@@ -152,7 +193,7 @@ const Row = ({
     return {
       transform: [
         {
-          translateY: dragging.value ? dragY.value : offsetY.value,
+          translateY: dragIndex.value === index ? dragY.value : offsetY.value,
         },
       ],
       shadowOpacity: dragging.value ? 0.5 : 0,
@@ -245,11 +286,11 @@ const DATA = [
   "react-native-learning",
   "balance-mobile",
   "android",
-  "shop-core-mobile",
-  "shopify",
-  "ios-retail",
-  "simon-says-rn",
-  "docusaurus",
-  "hydrogren",
-  "oxygen",
+  //   "shop-core-mobile",
+  //   "shopify",
+  //   "ios-retail",
+  //   "simon-says-rn",
+  //   "docusaurus",
+  //   "hydrogren",
+  //   "oxygen",
 ];
