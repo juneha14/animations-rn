8a15f18dc2f5dc108383f8f956b04b7cd7a04e1b
June Ha <june.ha14@gmail.com>
2022-02-24 14:48:21 -0800

    Better translation for non-drag row; Behaviour is such that if bottom of drag-row hits the middle of the row, then it moves up - and if top of drag-row hits middle of row, then it moves down

--- src/DragToSortList/DragToSortList.tsx
+++ src/DragToSortList/DragToSortList.tsx
@@ -1,29 +1,48 @@
 import React from "react";
 import { Image, Text, View } from "react-native";
 import Animated, {
-  runOnJS,
   useAnimatedReaction,
   useAnimatedStyle,
+  useDerivedValue,
   useSharedValue,
   withTiming,
 } from "react-native-reanimated";
-import {
-  Gesture,
-  GestureDetector,
-  RotationGestureHandler,
-} from "react-native-gesture-handler";
-import { Colors, Spacing } from "../utils";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { Colors, snapPoints, Spacing } from "../utils";
 import { Ionicons } from "@expo/vector-icons";
 
+const Images = {
+  profile: require("../../assets/docusaurus.png"),
+};
+
+const DATA = [
+  "arrive-client",
+  "ios",
+  "pos-next-react-native",
+  "media-db-rn",
+  "animations-rn",
+  "memory-matching-game-rn",
+  "react-native-learning",
+  "balance-mobile",
+  "android",
+  //   "shop-core-mobile",
+  //   "shopify",
+  //   "ios-retail",
+  //   "simon-says-rn",
+  //   "docusaurus",
+  //   "hydrogren",
+  //   "oxygen",
+];
+
 export const DragToSortList: React.FC = () => {
-  const absY = useSharedValue(0);
-  const velY = useSharedValue(0);
+  const dragAbsY = useSharedValue(0);
   const dragIndex = useSharedValue(-1);
 
   return (
     <Animated.ScrollView
       style={{ backgroundColor: Colors.SurfaceForegroundPressed }}
       contentContainerStyle={{
+        // marginVertical: Spacing.defaultMargin,
         backgroundColor: Colors.SurfaceForegroundPressed,
       }}
     >
@@ -32,12 +51,8 @@ export const DragToSortList: React.FC = () => {
           key={val}
           index={index}
           title={val}
-          absY={absY}
-          velY={velY}
+          dragAbsY={dragAbsY}
           dragIndex={dragIndex}
-          onBeginDrag={() => {
-            dragIndex.value = index;
-          }}
         />
       ))}
     </Animated.ScrollView>
@@ -45,7 +60,10 @@ export const DragToSortList: React.FC = () => {
 };
 
 const ROW_HEIGHT = 60;
-const NAV_BAR_HEIGHT = 111;
+const SNAP_POINTS = DATA.map((_, index) => [
+  index * ROW_HEIGHT,
+  -index * ROW_HEIGHT,
+]).flat();
 
 // onEnd gesture should snap the dragged row into proper position
 // onEnd gesture for dragged row outside the view's bounds should snap into position
@@ -54,42 +72,43 @@ const NAV_BAR_HEIGHT = 111;
 const Row = ({
   index,
   title,
-  absY,
-  velY,
   dragIndex,
-  onBeginDrag,
+  dragAbsY,
 }: {
   index: number;
   title: string;
-  absY: Animated.SharedValue<number>;
-  velY: Animated.SharedValue<number>;
+  dragAbsY: Animated.SharedValue<number>;
   dragIndex: Animated.SharedValue<number>;
-  onBeginDrag: () => void;
 }) => {
-  const activeRange = [index * ROW_HEIGHT, (index + 1) * ROW_HEIGHT];
+  // Values for handling the non-dragging rows
   const offsetY = useSharedValue(0);
+  const dragStartAbsY = useDerivedValue(() => dragIndex.value * ROW_HEIGHT);
 
-  const dragStartY = useSharedValue(0);
-  const dragY = useSharedValue(0);
+  // Values for handling the dragging row
+  const dragStartOffsetY = useSharedValue(0);
+  const dragOffsetY = useSharedValue(0);
   const dragging = useSharedValue(false);
   const zIndex = useSharedValue(0);
 
+  // Observe dragAbsY value (produced from the dragging row pan gesture)
+  // and animate up/down the other rows
   useAnimatedReaction(
-    () => Math.abs(absY.value - NAV_BAR_HEIGHT),
-    (y) => {
+    () => Math.abs(dragAbsY.value),
+    (dragEndPos) => {
       if (dragIndex.value === index || dragIndex.value === -1) return;
 
-      const dragStartPos = dragIndex.value * ROW_HEIGHT;
-      const dragEndPos = y;
+      const dragStartPos = dragStartAbsY.value;
       const dragDelta = dragStartPos - dragEndPos;
 
-      const rowMidPos = activeRange[0] + ROW_HEIGHT / 2;
-
       // Moved drag row up, so move my (index) row down
+      // Drag row is moving ABOVE the initial drag row starting position, regardless of whether it's up or down
       if (dragDelta > 0) {
+        // Determine the position where the row is deemed to be within the dragged zone
+        const rowActivePos = index * ROW_HEIGHT + ROW_HEIGHT / 2;
         const isRowInsideDraggedZone =
-          dragEndPos <= rowMidPos && dragStartPos >= rowMidPos;
+          dragEndPos <= rowActivePos && dragStartPos >= rowActivePos;
 
+        // If the row is positioned inside the drag zone, then move it down; Else, move it back to origin
         if (isRowInsideDraggedZone) {
           offsetY.value = withTiming(ROW_HEIGHT);
         } else {
@@ -97,10 +116,14 @@ const Row = ({
         }
       }
       // Moved drag row down, so move my (index) row up
+      // Drag row is moving BELOW the initial drag row starting position, regardless of whether it's up or down
       else {
+        // Determine the position where the row is deemed to be within the dragged zone
+        const rowActivePos = index * ROW_HEIGHT - ROW_HEIGHT / 2;
         const isRowInsideDraggedZone =
-          dragStartPos <= rowMidPos && dragEndPos >= rowMidPos;
+          dragStartPos <= rowActivePos && dragEndPos >= rowActivePos;
 
+        // If the row is positioned inside the drag zone, then move it up; Else, move it back to origin
         if (isRowInsideDraggedZone) {
           offsetY.value = withTiming(-ROW_HEIGHT);
         } else {
@@ -110,90 +133,24 @@ const Row = ({
     }
   );
 
-  //   useAnimatedReaction(
-  //     () => Math.abs(absY.value - NAV_BAR_HEIGHT),
-  //     (y) => {
-  //       if (dragIndex.value === index || dragIndex.value === -1) return;
-
-  //       const top = activeRange[0] + offsetY.value;
-  //       const bottom = activeRange[1] + offsetY.value;
-  //       const mid = top + ROW_HEIGHT / 2;
-
-  //       if (y >= top && y <= bottom) {
-  //         if (velY.value > 0) {
-  //           // Moving down
-  //           if (y + ROW_HEIGHT > mid) {
-  //             if (offsetY.value === 0) {
-  //               offsetY.value = withTiming(-ROW_HEIGHT);
-  //             } else if (offsetY.value === ROW_HEIGHT) {
-  //               offsetY.value = withTiming(0);
-  //             }
-  //           }
-  //         } else {
-  //           // Moving up
-  //           if (y <= mid) {
-  //             if (offsetY.value === 0) {
-  //               offsetY.value = withTiming(ROW_HEIGHT);
-  //             } else if (offsetY.value === -ROW_HEIGHT) {
-  //               offsetY.value = withTiming(0);
-  //             }
-  //           }
-  //         }
-  //       } else {
-  //         if (velY.value < -500) {
-  //           // Dragged up very quickly
-  //           console.log(
-  //             "========== File: DragToSortList.tsx, Line: 118 =========="
-  //           );
-  //           if (y < mid) {
-  //             if (offsetY.value === 0) {
-  //               offsetY.value = withTiming(ROW_HEIGHT, { duration: 100 });
-  //             } else if (offsetY.value === -ROW_HEIGHT) {
-  //               offsetY.value = withTiming(0, { duration: 100 });
-  //             }
-  //           }
-  //         } else if (velY.value > 500) {
-  //           console.log(
-  //             "========== File: DragToSortList.tsx, Line: 126 =========="
-  //           );
-  //           // Dragged down very quickly
-  //           if (y < mid) {
-  //             if (offsetY.value === 0) {
-  //               offsetY.value = withTiming(-ROW_HEIGHT, { duration: 100 });
-  //             } else if (offsetY.value === ROW_HEIGHT) {
-  //               offsetY.value = withTiming(0, { duration: 100 });
-  //             }
-  //           }
-  //         }
-  //       }
-  //     }
-  //   );
-
   const panGesture = Gesture.Pan()
     .onStart(() => {
-      dragStartY.value = dragY.value;
       dragging.value = true;
+      dragIndex.value = index;
+      dragStartOffsetY.value = dragOffsetY.value;
       zIndex.value = 1;
-      runOnJS(onBeginDrag)();
     })
     .onUpdate((e) => {
-      absY.value = e.absoluteY;
-      velY.value = e.velocityY;
-
-      dragY.value = dragStartY.value + e.translationY;
-    })
-    .onEnd(() => {
-      dragY.value = withTiming(0, { duration: 300 }, () => {
-        zIndex.value = 0;
-      });
-      dragging.value = false;
+      dragOffsetY.value = dragStartOffsetY.value + e.translationY;
+      dragAbsY.value = dragStartAbsY.value + dragOffsetY.value;
     });
 
   const aStyle = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          translateY: dragIndex.value === index ? dragY.value : offsetY.value,
+          translateY:
+            dragIndex.value === index ? dragOffsetY.value : offsetY.value,
         },
       ],
       shadowOpacity: dragging.value ? 0.5 : 0,
@@ -249,48 +206,11 @@ const Row = ({
             <Ionicons
               name="ios-menu-outline"
               size={25}
-              color={Colors.IconNeutral}
+              color={Colors.IconSubdued}
             />
           </View>
         </GestureDetector>
       </View>
-
-      <Divider />
     </Animated.View>
   );
 };
-
-const Divider = () => {
-  return (
-    <View
-      style={{
-        marginLeft: Spacing.defaultMargin + 40 + Spacing.l,
-        height: 1,
-        backgroundColor: Colors.BorderSubdued,
-      }}
-    />
-  );
-};
-
-const Images = {
-  profile: require("../../assets/docusaurus.png"),
-};
-
-const DATA = [
-  "arrive-client",
-  "ios",
-  "pos-next-react-native",
-  "media-db-rn",
-  "animations-rn",
-  "memory-matching-game-rn",
-  "react-native-learning",
-  "balance-mobile",
-  "android",
-  //   "shop-core-mobile",
-  //   "shopify",
-  //   "ios-retail",
-  //   "simon-says-rn",
-  //   "docusaurus",
-  //   "hydrogren",
-  //   "oxygen",
-];
