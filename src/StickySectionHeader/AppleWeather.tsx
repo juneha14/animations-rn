d30bbd9af2d13b7c8dd957208115497e86eb53ef
June Ha <june.ha14@gmail.com>
2022-03-11 15:50:41 -0800

    Header animation based on scrollY offset

--- src/StickySectionHeader/AppleWeather.tsx
+++ src/StickySectionHeader/AppleWeather.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Palette, Spacing } from "../utils";
 import { Header } from "./components/Header";
@@ -10,24 +14,37 @@ import { AirQuality } from "./components/AirQuality";
 import { UserActions, Footer } from "./components/UserActionFooter";
 
 export const AppleWeather: React.FC = () => {
-  const { top } = useSafeAreaInsets();
+  const { top, bottom } = useSafeAreaInsets();
+  const scrollY = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollY.value = e.contentOffset.y;
+      console.log("==== Value of scrollY.value:", scrollY.value);
+    },
+  });
 
   return (
-    <ScrollView
-      style={{ backgroundColor: Palette.Blue.L1 }}
-      contentContainerStyle={{
-        paddingHorizontal: Spacing.xl,
-        paddingVertical: top + Spacing.xl,
-      }}
-      showsVerticalScrollIndicator={false}
-    >
-      <Header />
-      <HourlyForecast />
-      <TenDayForecast />
-      <AirQuality />
-      <OtherMeteorlogyInfo />
-      <UserActions />
-      <Footer />
-    </ScrollView>
+    <View style={{ backgroundColor: Palette.Blue.L1 }}>
+      <Header scrollY={scrollY} />
+      <Animated.ScrollView
+        style={{ marginTop: top + 110, zIndex: 2 }}
+        contentContainerStyle={{
+          paddingHorizontal: Spacing.xl,
+          paddingTop: 110,
+          paddingBottom: bottom,
+        }}
+        showsVerticalScrollIndicator={false}
+        scrollEventThrottle={16}
+        onScroll={onScroll}
+      >
+        <HourlyForecast />
+        <TenDayForecast />
+        <AirQuality />
+        <OtherMeteorlogyInfo />
+        <UserActions />
+        <Footer />
+      </Animated.ScrollView>
+    </View>
   );
 };
--- src/StickySectionHeader/components/Header.tsx
+++ src/StickySectionHeader/components/Header.tsx
@@ -1,35 +1,119 @@
 import React from "react";
 import { Text, View } from "react-native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Palette } from "../../utils";
 
-export const Header = () => {
+export const Header = ({
+  scrollY,
+}: {
+  scrollY: Animated.SharedValue<number>;
+}) => {
+  const { top } = useSafeAreaInsets();
+
+  const headerAStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: interpolate(
+            scrollY.value,
+            [-150, 0, 150],
+            [30, 0, -30],
+            Extrapolate.CLAMP
+          ),
+        },
+      ],
+    };
+  });
+
+  const currentTempAStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [95, 120], [1, 0]),
+    };
+  });
+
+  const highLowTempAStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [0, 120], [1, 0]),
+    };
+  });
+
+  const forecastSummaryAStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [120, 150], [0, 1]),
+    };
+  });
+
   return (
-    <View
+    <Animated.View
       style={[
-        { justifyContent: "center", alignItems: "center", marginTop: 30 },
+        {
+          position: "absolute",
+          top: 0,
+          left: 0,
+          right: 0,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: top + 50,
+          zIndex: 1,
+        },
+        headerAStyle,
       ]}
     >
       <Text style={{ color: Palette.White, fontSize: 35 }}>
         Greater Vancouver
       </Text>
-      <Text
+
+      <Animated.Text
+        style={[{ color: Palette.White, fontSize: 18 }, forecastSummaryAStyle]}
+      >
+        4° | Mostly Sunny
+      </Animated.Text>
+
+      <View
         style={{
-          color: Palette.White,
-          fontSize: 90,
-          fontWeight: "200",
-          marginVertical: -10,
-          marginLeft: 10,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: -25,
         }}
       >
-        4°
-      </Text>
-      <Text style={{ color: Palette.White, fontSize: 20, fontWeight: "500" }}>
-        Mostly Sunny
-      </Text>
-      <Text style={{ color: Palette.White, fontSize: 20, fontWeight: "500" }}>
-        H: 7° L: 2°
-      </Text>
-      <View />
-    </View>
+        <Animated.Text
+          style={[
+            {
+              color: Palette.White,
+              fontSize: 90,
+              fontWeight: "200",
+              marginVertical: -10,
+              marginLeft: 10,
+            },
+            currentTempAStyle,
+          ]}
+        >
+          4°
+        </Animated.Text>
+
+        <Animated.View
+          style={[
+            { justifyContent: "center", alignItems: "center" },
+            highLowTempAStyle,
+          ]}
+        >
+          <Text
+            style={{ color: Palette.White, fontSize: 20, fontWeight: "500" }}
+          >
+            Mostly Sunny
+          </Text>
+          <Text
+            style={{ color: Palette.White, fontSize: 20, fontWeight: "500" }}
+          >
+            H: 7° L: 2°
+          </Text>
+        </Animated.View>
+      </View>
+    </Animated.View>
   );
 };
