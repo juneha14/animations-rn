eee293f6b7701643db769f1835c54fe34b89dad3
June Ha <june.ha14@gmail.com>
2022-03-21 14:51:00 -0700

    Clean up

--- src/CardWallet/CardWallet.tsx
+++ src/CardWallet/CardWallet.tsx
@@ -1,12 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Image,
-  ScrollView,
-  Dimensions,
-  Pressable,
-} from "react-native";
+import { View, Text, Image, ScrollView, Pressable } from "react-native";
 import Animated, {
   Extrapolate,
   interpolate,
@@ -16,8 +9,22 @@ import Animated, {
   withDelay,
   withTiming,
 } from "react-native-reanimated";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  SNAP_OFFSETS,
+  CARDS,
+  CARD_WIDTH,
+  CARD_HEIGHT,
+  EMPTY_OFFSET,
+  CARD_MARGIN,
+  WIDTH,
+  usePayments,
+  PAYMENTS,
+  Payment,
+  boxColorForStatus,
+  amountColorForStatus,
+} from "./utils";
 import { Colors, Spacing } from "../utils";
+import { Ionicons } from "@expo/vector-icons";
 
 export const CardWallet: React.FC = () => {
   return (
@@ -67,29 +74,6 @@ const CardCarousel = () => {
     </View>
   );
 };
-
-const CARDS = [
-  { card: require("./assets/card1.png") },
-  { card: require("./assets/card2.png") },
-  { card: require("./assets/card3.png") },
-  { card: require("./assets/card4.png") },
-  { card: require("./assets/card5.png") },
-  { card: require("./assets/card6.png") },
-];
-
-const WIDTH = Dimensions.get("window").width;
-const PAGE_PADDING = 2 * Spacing.defaultMargin;
-
-const RATIO = 228 / 362;
-const CARD_WIDTH = (WIDTH - PAGE_PADDING) * 0.8;
-const CARD_HEIGHT = CARD_WIDTH * RATIO;
-
-const CARD_MARGIN = 0;
-const EMPTY_OFFSET = (WIDTH - PAGE_PADDING - CARD_WIDTH - CARD_MARGIN) / 2;
-
-const SNAP_OFFSETS = CARDS.map(
-  (_, index) => index * (CARD_WIDTH + CARD_MARGIN)
-);
 
 const Card = ({
   index,
@@ -560,179 +544,4 @@ const LoanDetailRow = ({ title, value }: { title: string; value: string }) => {
       <Text style={{ fontWeight: "500", fontSize: 17 }}>{value}</Text>
     </View>
   );
-};
-
-/* Models and Helpers */
-
-interface SummaryContent {
-  type: "content";
-  data: Payment;
-}
-interface ShowAction {
-  type: "action";
-  title: string;
-  onPress: () => void;
-}
-type PaymentRowViewState = SummaryContent | ShowAction;
-
-const usePayments = (payments: Payment[]) => {
-  const [viewState, setViewState] = useState<PaymentRowViewState[]>([]);
-
-  useEffect(() => {
-    const createViewState = () => {
-      const isAllPaid = payments.every((p) => p.status === "paid");
-      const isAllOverdue = payments.every((p) => p.status === "overdue");
-      const isAllUpcoming = payments.every((p) => p.status === "upcoming");
-      if (isAllPaid || isAllOverdue || isAllUpcoming) {
-        setViewState(payments.map((p) => ({ type: "content", data: p })));
-        return;
-      }
-
-      // Find the first upcoming payment
-      let firstUpcomingIndex = 0;
-      for (let i = 0; i < payments.length; i++) {
-        const payment = payments[i];
-        if (payment.status === "upcoming") {
-          firstUpcomingIndex = i;
-          break;
-        }
-      }
-
-      // Not enough payment transactions to warrant showing the in-line 'Show more' action button
-      if (firstUpcomingIndex < 5) {
-        setViewState(payments.map((p) => ({ type: "content", data: p })));
-        return;
-      }
-
-      const previousPayments: SummaryContent[] = payments
-        .slice(1, firstUpcomingIndex)
-        .map((p) => ({ type: "content", data: p }));
-
-      const nextPayments: SummaryContent[] = payments
-        .slice(firstUpcomingIndex)
-        .map((p) => ({ type: "content", data: p }));
-
-      // Create view state with the in-line 'Show more' action button
-      let viewState: PaymentRowViewState[] = [];
-      viewState.push({ type: "content", data: payments[0] });
-      viewState.push({
-        type: "action",
-        title: "Show previous payments",
-        onPress: () => {
-          setViewState((prev) => {
-            return [prev[0], ...previousPayments, ...prev.slice(2)];
-          });
-        },
-      });
-      viewState = [...viewState, ...nextPayments];
-      setViewState(viewState);
-    };
-
-    createViewState();
-  }, [payments]);
-
-  return {
-    viewState,
-  };
-};
-
-type PaymentStatus = "paid" | "upcoming" | "overdue";
-interface Payment {
-  paidDate: string;
-  scheduledDate: string;
-  cardTender: string;
-  amount: number;
-  status: PaymentStatus;
-}
-
-const PAYMENTS: Payment[] = [
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "paid",
-  },
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "paid",
-  },
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "paid",
-  },
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "paid",
-  },
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "paid",
-  },
-  {
-    paidDate: "August 1, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "Adjustment",
-    amount: -1.22,
-    status: "paid",
-  },
-  {
-    paidDate: "August 9, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "overdue",
-  },
-  {
-    paidDate: "August 12, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "upcoming",
-  },
-  {
-    paidDate: "August 12, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "upcoming",
-  },
-  {
-    paidDate: "August 12, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "upcoming",
-  },
-  {
-    paidDate: "August 12, 2021",
-    scheduledDate: "August 9, 2021",
-    cardTender: "VISA 1234",
-    amount: 165.63,
-    status: "upcoming",
-  },
-];
-
-const amountColorForStatus: Record<PaymentStatus, string> = {
-  paid: Colors.TextSubdued,
-  upcoming: Colors.TextNeutral,
-  overdue: Colors.TextCritical,
-};
-
-const boxColorForStatus: Record<PaymentStatus, string> = {
-  paid: Colors.IconSuccess,
-  upcoming: Colors.IconSubdued,
-  overdue: Colors.IconCritical,
 };
