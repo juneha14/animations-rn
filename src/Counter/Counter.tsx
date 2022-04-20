ecdb7857d3c91fa4f823023c937d764ca96e2aea
June Ha <june.ha14@gmail.com>
2022-04-20 13:43:09 -0700

    Add slide pan gesture to increment/decrement quicker

--- src/Counter/Counter.tsx
+++ src/Counter/Counter.tsx
@@ -2,29 +2,94 @@ import React, { useState } from "react";
 import { Pressable, ScrollView, View } from "react-native";
 import Animated, {
   runOnJS,
-  useAnimatedScrollHandler,
+  useAnimatedReaction,
   useAnimatedStyle,
   useSharedValue,
+  withDelay,
   withSequence,
   withTiming,
 } from "react-native-reanimated";
-import { Colors, Spacing } from "../utils";
-import { EvilIcons } from "@expo/vector-icons";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { clamp, Colors, Spacing } from "../utils";
+import { EvilIcons } from "@expo/vector-icons";
 
 export const Counter: React.FC = () => {
   const [count, setCount] = useState(0);
+  const [isAnimationComplete, setIsAnimationComplete] = useState(true);
 
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
 
   const panGesture = Gesture.Pan()
     .onUpdate((e) => {
-      translateX.value = e.translationX;
+      translateX.value = clamp(e.translationX, -64, 64);
     })
     .onEnd(() => {
       translateX.value = withTiming(0);
     });
+
+  const increment = (completion?: () => void) => {
+    "worklet";
+    translateY.value = withDelay(
+      completion ? 300 : 0,
+      withTiming(-80, undefined, () => {
+        translateY.value = withSequence(
+          withTiming(80, { duration: 0 }),
+          withTiming(0, undefined, () => {
+            completion && completion();
+          })
+        );
+
+        runOnJS(setCount)(count + 1);
+      })
+    );
+  };
+
+  const decrement = (completion?: () => void) => {
+    "worklet";
+    translateY.value = withDelay(
+      completion ? 300 : 0,
+      withTiming(80, undefined, () => {
+        translateY.value = withSequence(
+          withTiming(-80, { duration: 0 }),
+          withTiming(0, undefined, () => {
+            completion && completion();
+          })
+        );
+
+        runOnJS(setCount)(count - 1);
+      })
+    );
+  };
+
+  useAnimatedReaction(
+    () => translateX.value,
+    (x) => {
+      // Increment
+      if (x > 30 && x < 60) {
+        if (isAnimationComplete) {
+          runOnJS(setIsAnimationComplete)(false);
+          increment(() => {
+            runOnJS(setIsAnimationComplete)(true);
+          });
+        }
+      } else if (x > 60) {
+        increment();
+      }
+      // Decrement
+      else if (x < -30 && x > -60) {
+        if (isAnimationComplete) {
+          runOnJS(setIsAnimationComplete)(false);
+          decrement(() => {
+            runOnJS(setIsAnimationComplete)(true);
+          });
+        }
+      } else if (x < -60) {
+        decrement();
+      }
+    },
+    [count, isAnimationComplete]
+  );
 
   const counterAStyle = useAnimatedStyle(() => {
     return {
@@ -52,21 +117,9 @@ export const Counter: React.FC = () => {
         style={{
           flexDirection: "row",
           alignItems: "center",
-          overflow: "hidden",
         }}
       >
-        <Pressable
-          onPress={() => {
-            translateY.value = withTiming(80, undefined, () => {
-              translateY.value = withSequence(
-                withTiming(-80, { duration: 0 }),
-                withTiming(0)
-              );
-
-              runOnJS(setCount)(count - 1);
-            });
-          }}
-        >
+        <Pressable onPress={() => decrement()}>
           <EvilIcons name="minus" size={40} color={Colors.IconNeutral} />
         </Pressable>
 
@@ -81,6 +134,8 @@ export const Counter: React.FC = () => {
                 marginHorizontal: Spacing.xl,
                 borderRadius: 10,
                 backgroundColor: Colors.SurfaceHighlight,
+                zIndex: 1,
+                overflow: "hidden",
               },
               counterAStyle,
             ]}
@@ -96,18 +151,7 @@ export const Counter: React.FC = () => {
           </Animated.View>
         </GestureDetector>
 
-        <Pressable
-          onPress={() => {
-            translateY.value = withTiming(-80, undefined, () => {
-              translateY.value = withSequence(
-                withTiming(80, { duration: 0 }),
-                withTiming(0)
-              );
-
-              runOnJS(setCount)(count + 1);
-            });
-          }}
-        >
+        <Pressable onPress={() => increment()}>
           <EvilIcons name="plus" size={40} color={Colors.IconNeutral} />
         </Pressable>
       </View>
